27b21718066faa42c1fbb3b771388d83c6d44ae1
Alexandre Fernandez <contact@alexandrefernandez.me>
2022-02-02 16:15:42 +0100

    displayUserInHome hotfix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,13 +13,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(UserRepository $userRepo): Response
+    public function index(): Response
     {
-		$user = $this->getUser()->getUserIdentifier();
-
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-			"user" => $user
+            'controller_name' => 'HomeController'
         ]);
     }
 }
