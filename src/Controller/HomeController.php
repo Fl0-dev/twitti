ef43e8d8455d3ef588acbdda089f7faedd68b4e2
Alexandre Fernandez <contact@alexandrefernandez.me>
2022-02-02 15:47:25 +0100

    displayUserInHome feature (added display in header instead of home)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepo): Response
     {
+		$user = $this->getUser()->getUserIdentifier();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+			"user" => $user
         ]);
     }
 }
