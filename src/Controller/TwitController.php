8ae3c966edcab07899953939ba437cdd77c53ebf
adamLefrancois <lefrancois.adam@gmail.com>
2022-02-02 14:22:11 +0100

    twitController

--- src/Controller/TwitController.php
+++ src/Controller/TwitController.php
@@ -6,17 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class TwitController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/twit", name="twit")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('twit/index.html.twig', [
+            'controller_name' => 'TwitController',
         ]);
     }
-
-
 }
